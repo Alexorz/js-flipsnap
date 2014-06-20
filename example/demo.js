2c229ed206f2c150645e48f07bef374c8e26b980
binlong.libl <binlong.libl@alibaba-inc.com>
2014-06-20 15:26:49 +0800

    Add infinity loop feature & min.

--- example/demo.js
+++ example/demo.js
@@ -7,7 +7,9 @@ if (!$('.demo').length) return;
 })();
 
 (function img() {
-	Flipsnap('#demo-img .flipsnap');
+	Flipsnap('#demo-loop .flipsnap', {
+		loop: true
+	});
 })();
 
 (function distance() {
@@ -126,7 +128,7 @@ if (!$('.demo').length) return;
             var offsetRatio = Math.abs(ev.absoluteX + itemWidth * i) / itemWidth;
             offsetRatio = ( offsetRatio > 1 ? 1 : offsetRatio );
             if ( offsetRatio != 0 || offsetRatio != 1 || offsetRatio != ratioCache[ i ] ) {
-                flipsnap.setStyle( items[i].style, 'transform', flipsnap.use3d ? 
+                flipsnap.setStyle( items[i].style, 'transform', flipsnap.use3d ?
 				  'perspective(200px) translate3d('+ offsetRatio * side * -1 * offsetX +'px, 0, '+  offsetRatio * -1 * translateZ +'px)'
             	: 'scale('+ (1 - offsetRatio * 0.5) +')' );
                 ratioCache[ i ] = offsetRatio;
