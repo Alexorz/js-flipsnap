1f79754e78845de7ef4684c8048156db9d86c43a
Alexius Lee <lee.alexorz@gmail.com>
2014-07-07 16:47:06 +0800

    Fix IE 11 auto-play bug.

--- flipsnap.js
+++ flipsnap.js
@@ -284,9 +284,12 @@ Flipsnap.prototype.toNext = function(transitionDuration) {
   if ( self.loop && self.currentPoint >= self.maxPoint ) {
     self.animation = false;
     self._setStyle({ transitionDuration: '0ms' });
-    self._setX(0, 0);
+    // Timeout for IE 11
     setTimeout(function(){
-      self.moveToPoint(0, transitionDuration);
+      self._setX(0, 0);
+      setTimeout(function(){
+        self.moveToPoint(0, transitionDuration);
+      }, 0);
     }, 0);
   }
   else {
@@ -304,9 +307,11 @@ Flipsnap.prototype.toPrev = function(transitionDuration) {
   if ( self.loop && self.currentPoint === 0 ) {
     self.animation = false;
     self._setStyle({ transitionDuration: '0ms' });
-    self._setX(self._maxX, 0);
     setTimeout(function(){
-      self.moveToPoint(self.maxPoint, transitionDuration);
+      self._setX(self._maxX, 0);
+      setTimeout(function(){
+        self.moveToPoint(self.maxPoint, transitionDuration);
+      }, 0);
     }, 0);
   }
   else {
